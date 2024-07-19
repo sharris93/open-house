08be194306b4a1d12fdeedc808241db863fca188
Sam Harris <sharrisdev@gmail.com>
2024-07-19 12:30:31 +0100

    testing dynamic image prefixing

--- middleware/pass-user-to-view.js
+++ middleware/pass-user-to-view.js
@@ -1,5 +1,6 @@
 const passUserToView = (req, res, next) => {
   res.locals.user = req.session.user ? req.session.user : null;
+  res.locals.imagePrefix = process.env.PRODUCTION === 'true' ? '/public' : ''
   next();
 };
 
